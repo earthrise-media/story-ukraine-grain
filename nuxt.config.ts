6d97be23800c26be3a1ab48f85099a0c8bcfc0da
EJ Fox <ejfox@ejfox.com>
2023-02-02 11:05:29 -0500

    feat: add turf, topojson, and ukraine geojson

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  vite: {
+    define: {
+      'process.env.POLYGON_CLIPPING_MAX_QUEUE_SIZE': '1000000',
+      'process.env.POLYGON_CLIPPING_MAX_SWEEPLINE_SEGMENTS': '1000000',
+    },
+  },
 })
--- pages/map.vue
+++ pages/map.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- Eventually this should all be moved to a component, but for now we don't need to have a ton of files for prototypes -->
 
-
+    <svg id="map" ref="mapSvg"></svg>
   </div>
 </template>
 <script setup>
@@ -11,4 +11,43 @@ import * as d3 from 'd3'
 import * as topojson from 'topojson'
 import * as turf from '@turf/turf'
 
+const mapSvg = ref(null)
+
+onMounted(() => {
+  // load geojson data from public/data/stanford-ukraine-geojson.json with d3
+  d3.json('/data/stanford-ukraine-geojson.json').then((data) => {
+    // Our geojson is contained in data.objects.stanford-pp624tm0074-geojson
+    // We need to convert it to a feature collection
+    const featureCollection = topojson.feature(data, data.objects['stanford-pp624tm0074-geojson'])
+    
+    // get the width and height of the SVG
+    const width = mapSvg.value.clientWidth
+    const height = mapSvg.value.clientHeight
+
+    // create a projection
+    const projection = d3.geoMercator()
+      .fitSize([width, height], featureCollection)
+
+    // create a path generator
+    const path = d3.geoPath()
+      .projection(projection)
+
+    // create a geojson layer
+    const geojsonLayer = d3.select(mapSvg.value)
+      .selectAll('path')
+      .data(featureCollection.features)
+      .join('path')
+      .attr('d', path)
+      .attr('fill', 'steelblue')
+      .attr('stroke', 'white')
+  })
+})
+
+
 </script>
+<style>
+#map {
+  width: 100%;
+  height: 80vh;
+}
+</style>
