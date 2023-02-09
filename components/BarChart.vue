1d8d4fffb0202e93b82427b0a7a00706db879997
EJ Fox <ejfox@ejfox.com>
2023-02-09 18:50:09 -0500

    refactor: set up props for components

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -1,2 +1,21 @@
 <template></template>
-<script setup></script>
+<script setup>
+const props = defineProps({
+  config: {
+    type: Object,
+    required: true,
+  },
+  scenario: {
+    type: Object,
+    required: false,
+  },
+  oblastData: {
+    type: Array,
+    required: true,
+  },
+  width: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
--- components/SankeyChart.vue
+++ components/SankeyChart.vue
@@ -3,4 +3,30 @@
     <!-- sankey stuff -->
   </svg>
 </template>
-<script setup></script>
+<script setup>
+/*
+      <SankeyChart
+        v-if="sankeyConfig.visibility"
+        ref="sankeyChart"
+        :config="sankeyConfig"
+        :importExportData="importExportData"
+        :width="graphicWidth"
+      />
+      */
+
+// set up props
+const props = defineProps({
+  config: {
+    type: Object,
+    required: true,
+  },
+  importExportData: {
+    type: Array,
+    required: true,
+  },
+  width: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
