29381b9b47ce0243386ad04ebdb480acfec683c2
EJ Fox <ejfox@ejfox.com>
2023-03-08 14:55:33 -0500

    refactor: move scrollytelling to index

---
+++ pages/old_index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <!-- a list of prototypes created so far -->
+    <ul>
+      <li v-for="prototype in prototypes" :key="prototype">
+        <NuxtLink :to="`/${prototype}`">{{ prototype }}</NuxtLink>
+      </li>
+    </ul>
+  </main>
+</template>
+<script setup>
+const prototypes = ["map", "scatterplot", "scrollytelling"];
+</script>
